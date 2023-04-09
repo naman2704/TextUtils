4dc8ff0ad4224b4a036066e9dc63320e6767ec2e
naman2704 <ns8000016@gmail.com>
2023-04-09 23:27:08 +0530

    Added some more controls

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function TextForm(props) {
   const [text, setText] = useState("");
@@ -13,22 +13,53 @@ export default function TextForm(props) {
   }
   const clearTextBox = (event) => {
     setText("");
+    document.getElementById('textanalyzer').focus();
   }
+  const wordsCount = () => {
+    if (text.trim() === "") return 0;
+    else {
+        let words = text.split(" ");
+        let count = words.length;
+        words.forEach(word => {
+            if(word === "") count--;
+        });
+        return count;
+    }
+  }
+  const readTime = () => {
+    const time = 0.0008 * wordsCount();
+    if (time < 1) return `${time*60} secs`;
+    else if (time >= 1 && time <= 60) return `${time} mins`;
+    return `${Number.parseInt(time/60)} hrs ${Number.parseInt(time%60)} mins ${Number.parseInt((time%60)%60)} secs`;
+  }
+  useEffect(() => {
+    document.getElementById('textanalyzer').focus();
+  }, []);
   return (
-    <div class="container">
-    <label for="textanalyzer" className='display-2 my-3 fw-semibold'>{props.label}</label>
-    <textarea class="form-control" value={text} onChange={handleChange} placeholder={props.placeHolder} id="textanalyzer" rows="10"></textarea>
-    <ul class="list-group list-group-horizontal my-2">
-        <li class="list-group-item">
-            <button className="btn btn-primary" onClick={handleUpClick}>Uppercase</button>
-        </li>
-        <li class="list-group-item">
-            <button className="btn btn-primary" onClick={handleLoClick}>Lowercase</button>
-        </li>
-        <li class="list-group-item">
-            <button className="btn btn-primary" onClick={clearTextBox}>clear</button>
-        </li>
-    </ul>
+    <>
+    <div className="container">
+        <label htmlFor="textanalyzer" className='display-2 my-3 fw-semibold'>{props.label}</label>
+        <textarea className="form-control" value={text} onChange={handleChange} placeholder={props.placeHolder} id="textanalyzer" rows="6"></textarea>
+        <div className="btn-group my-3" role="group" aria-label="Basic example">
+            <button className="btn btn-light" onClick={handleUpClick}>Uppercase</button>
+            <button className="btn btn-light mx-2" onClick={handleLoClick}>Lowercase</button>
+            <button className="btn btn-light" onClick={clearTextBox}>clear</button>
+        </div>
     </div>
+    <div className="container text-style-controls">
+        <div className="btn-group my-3" role="group" aria-label="Basic example">
+            <button className="btn btn-light fw-bold">A</button>
+            <button className="btn btn-light fw-bold">B</button>
+            <button className="btn btn-light fst-italic">I</button>
+            <button className="btn btn-light"><u>U</u></button>
+        </div>
+    </div>
+    <div className="container text-summary my-2">
+        <p> <b>{wordsCount()}</b> words & <b>{text.length}</b> characters</p>
+        <p> <b>{readTime()}</b> required to read </p>
+        <h2>Text Preview</h2>
+        <p className="bg-secondary" style={{ color: "#ffffff" }}>{text}</p>
+    </div>
+    </>
   );
 }
